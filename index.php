df232ade2a8526487fd6dc651434e58c03aeb1f5
unknown <a.andjib@yahoo.fr>
2019-08-11 14:23:11 +0200

    findAllArticles() fait la connexion a laa base et la selection des articles

--- index.php
+++ index.php
@@ -6,25 +6,18 @@
  * On va donc se connecter à la base de données, récupérer les articles du plus récent au plus ancien (SELECT * FROM articles ORDER BY created_at DESC)
  * puis on va boucler dessus pour afficher chacun d'entre eux
  */
-require_once('libraries/database.php'); // connexion a la base de donnee
+require_once('libraries/database.php'); // interaction avec  la base de donnee
 require_once('libraries/utils.php'); // appel des vues
-/**
- * 
- * 1. Connexion à la base de données avec PDO
- * Attention, on précise ici deux options :
- * - Le mode d'erreur : le mode exception permet à PDO de nous prévenir violament quand on fait une connerie ;-)
- * - Le mode d'exploitation : FETCH_ASSOC veut dire qu'on exploitera les données sous la forme de tableaux associatifs
- * 
- */
-$pdo = getPDO();
+
 
 /**
+* 1. Connexion à la base de données:  getPDO() dans la function findAllArticles()
 * 2. Récupération des articles
+*
+* cf database.php 
 */
-// On utilisera ici la méthode query (pas besoin de préparation car aucune variable n'entre en jeu)
-$resultats = $pdo->query('SELECT * FROM articles ORDER BY created_at DESC');
-// On fouille le résultat pour en extraire les données réelles
-$articles = $resultats->fetchAll();
+
+$articles = findAllArticles();
 
 /**
 * 3. Affichage
--- libraries/database.php
+++ libraries/database.php
@@ -8,9 +8,7 @@
 */
 
 function getPDO()
-
 { 
-
     $pdo = new PDO('mysql:host=localhost;dbname=blogpoo;charset=utf8', 'root', '', [
         PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
         PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
@@ -18,5 +16,25 @@ function getPDO()
 
     return $pdo;
 }
+
+/**
+* Retourne la liste des aricles classes par date 
+*
+* @return array
+*/
+
+function findAllArticles()
+{
+    $pdo = getPDO();
+    // On utilisera ici la méthode query (pas besoin de préparation car aucune variable n'entre en jeu)
+    $resultats = $pdo->query('SELECT * FROM articles ORDER BY created_at DESC');
+    // On fouille le résultat pour en extraire les données réelles
+    $articles = $resultats->fetchAll();
+
+    return $articles;
+
+}
+
+
 
 ?>
