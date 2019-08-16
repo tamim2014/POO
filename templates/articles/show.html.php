5859e8bfaab5029a15b3034795f8207fb99da9da
unknown <a.andjib@yahoo.fr>
2019-08-16 12:36:47 +0200

    HOVER TABS pour faciliter la navigation - c'est pas le sujet , c'juste pratique!

--- templates/articles/show.html.php
+++ templates/articles/show.html.php
@@ -5,7 +5,7 @@
 <?= $article['content'] ?>
 
 <?php if (count($commentaires) === 0) : ?>
-    <h2>Il n'y a pas encore de commentaires pour cet article ... SOYEZ LE PREMIER ! :D</h2>
+    <h2>Il n'y a pas encore de commentaires pour cet article ... </h2>
 <?php else : ?>
     <h2>Il y a déjà <?= count($commentaires) ?> réactions : </h2>
     <?php foreach ($commentaires as $commentaire) : ?>
@@ -19,7 +19,7 @@
 <?php endif ?>
 
 <form action="save-comment.php" method="POST">
-    <h3>Vous voulez réagir ? N'hésitez pas les bros !</h3>
+
     <input type="text" name="author" placeholder="Votre pseudo !">
     <textarea name="content" id="" cols="30" rows="10" placeholder="Votre commentaire ..."></textarea>
     <input type="hidden" name="article_id" value="<?= $article_id ?>">
--- templates/layout.html.php
+++ templates/layout.html.php
@@ -5,13 +5,30 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="css/HoverTabs.css" media="all">
+    <link rel="stylesheet" href="css/content.css" media="all">
     <title>Mon superbe blog - <?= $pageTitle ?></title> <!-- variable defini dans index.php -->
 </head>
 
 <body>
-    <?= $pageContent ?> <!-- variable defini dans index.php: par appel de utils.php -->
-    <br><br><br>
-    <div style="text-align:center;"> <a  href="documentation/index.html">Documentation du projet</a></div>
+    <!-- Hover Tabs #efefef -->
+    <div id="documenter_sidebar">
+		<a href="#documenter_cover" id="documenter_logo" ></a>
+		<ol id="documenter_nav" >
+			<li ><a  class="current" href="#documenter_cover">Accueil</a></li>
+            <li><a  href="#"> Trading Algorithmique</a></li>
+			<li><a href="#">Rente immobilière</a></li>
+            <li><a href="#">Muses</a></li>
+			<li><a href="documentation/index.html">Documentation du projet</a></li>
+		</ol>
+		<div id="documenter_copyright">Programmation Orientee Objet<br></div>
+	</div>
+
+
+   <!-- Contenu -->
+   <div id="documenter_content">
+        <?= $pageContent ?> <!-- variable defini dans index.php: par appel de utils.php -->
+   </div>
 </body>
 
 </html>
