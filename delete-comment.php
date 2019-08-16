17f256d3134fc0db0be3554cda4935576758633b
unknown <a.andjib@yahoo.fr>
2019-08-16 19:21:07 +0200

    Etapr2.2 Controleur des commentaires

--- delete-comment.php
+++ delete-comment.php
@@ -7,58 +7,8 @@
  * Puis on le supprimera !
  */
 
-require_once('libraries/database.php');
-require_once('libraries/utils.php');
-require_once('libraries/models/Comment.php');// La class "Comment.php" aulieu de la bib "database.php"
+require_once('libraries/controllers/Comment.php'); 
 
-$model = new Comment();
-
-/**
- * 1. Récupération de l' "id" du paramètre en GET
- */
-if (empty($_GET['id']) || !ctype_digit($_GET['id'])) {
-    die("Ho ! Fallait préciser le paramètre id en GET !");
-}
-
-$id = $_GET['id'];
-
-
-/**
- * 2. Connexion à la base de données avec PDO
- * Attention, on précise ici deux options :
- * - Le mode d'erreur : le mode exception permet à PDO de nous prévenir violament quand on fait une connerie ;-)
- * - Le mode d'exploitation : FETCH_ASSOC veut dire qu'on exploitera les données sous la forme de tableaux associatifs
- * 
- * PS : Vous remarquez que ce sont les mêmes lignes que pour l'index.php ?!
- */
-$pdo = getPDO();
-
-/**
- * 3. Vérification de l'existence du commentaire
- */
-
-$commentaire = $model->find($id);
-if (!$commentaire) {
-    die("Aucun commentaire n'a l'identifiant $id !");
-}
-
-/**
- * 4. Suppression réelle du commentaire
- * On récupère l'identifiant de l'article avant de supprimer le commentaire
- */
-
-//$commentaire = $query->fetch();
-$article_id = $commentaire['article_id'];
-
-$model->delete($id); 
-
-/**
- * 5. Redirection vers l'article en question
- * 
- *  redirect("Location: article.php?id=" . $article_id);
- */
-
- header("Location: article.php?id=" . $article_id);
- exit();
-
+$controller = new \Controllers\Comment();
+$controller->delete();
 
---
+++ libraries/controllers/Comment.php
@@ -0,0 +1,90 @@
+<?php
+
+namespace Controllers;
+
+require_once('libraries/utils.php');
+require_once('libraries/models/Article.php');// La class "Article.php" aulieu de la bib "database.php"
+require_once('libraries/models/Comment.php');// La class "Comment.php" aulieu de la bib "database.php"
+
+
+class Comment 
+{
+    protected $model;
+
+    public function __construct(){
+        $this->model = new  \Models\Comment();
+    }
+
+    public function insert(){
+
+        $articleModel = new \Models\Article();
+
+        $author = null;
+        if (!empty($_POST['author'])) {
+            $author = $_POST['author'];
+        }
+
+
+        $content = null;
+        if (!empty($_POST['content'])) {
+
+            $content = htmlspecialchars($_POST['content']);
+        }
+
+
+        $article_id = null;
+        if (!empty($_POST['article_id']) && ctype_digit($_POST['article_id'])) {
+            $article_id = $_POST['article_id'];
+        }
+
+
+        if (!$author || !$article_id || !$content) {
+            die("Votre formulaire a été mal rempli !");
+        }
+
+
+        $pdo = getPDO();
+
+
+        $article = $articleModel->find($article_id);
+
+
+        if (!$article) {
+            die("Ho ! L'article $article_id n'existe pas boloss !");
+        }
+
+
+        $this->model->insertComment($author, $content, $article_id);
+
+        header('Location: article.php?id=' . $article_id);
+        exit();
+
+
+    }
+
+    public function delete(){
+
+        if (empty($_GET['id']) || !ctype_digit($_GET['id'])) {
+            die("Ho ! Fallait préciser le paramètre id en GET !");
+        }
+
+        $id = $_GET['id'];
+
+
+        $pdo = getPDO();
+
+        $commentaire = $this->model->find($id);
+        if (!$commentaire) {
+            die("Aucun commentaire n'a l'identifiant $id !");
+        }
+
+        $article_id = $commentaire['article_id'];
+        $this->model->delete($id); 
+        // redirect("Location: article.php?id=" . $article_id);
+        header("Location: article.php?id=" . $article_id);
+        exit();
+
+
+    }
+
+}
