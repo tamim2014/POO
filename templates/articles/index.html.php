3765b587410b4d26109c91a70f5b2b31a50538ba
unknown <a.andjib@yahoo.fr>
2019-08-16 13:57:52 +0200

    Notes: style .resume defini dans content.css pour la vue index.html.php

--- templates/articles/index.html.php
+++ templates/articles/index.html.php
@@ -1,11 +1,13 @@
 <h1>Nos articles</h1><br>
 
 <?php foreach ($articles as $article) : ?>
+<div class="resume" >
     <h2><?= $article['title'] ?></h2>
     <small>Ecrit le <?= $article['created_at'] ?></small>
     <p>
        <?= $article['introduction'] ?><br>
        <a  href="article.php?id=<?= $article['id'] ?>">Lire la suite</a> &nbsp; &nbsp;    
        <a href="delete-article.php?id=<?= $article['id'] ?>" onclick="return window.confirm(`Êtes vous sur de vouloir supprimer cet article ?!`)">Supprimer</a>
-    </p>    
+    </p> 
+</div>       
 <?php endforeach ?>
