29bdbd8ddf13a98bd15322b7ef721bb404b694cf
unknown <a.andjib@yahoo.fr>
2019-09-08 15:30:15 +0200

    ..

--- index.php
+++ index.php
@@ -6,7 +6,8 @@
  * On va donc se connecter à la base de données, récupérer les articles du plus récent au plus ancien (SELECT * FROM articles ORDER BY created_at DESC)
  * puis on va boucler dessus pour afficher chacun d'entre eux
  */
-require_once('libraries/autoload.php'); 
+require_once('libraries/autoload.php');
+// le \ signifie n'appartient  pas aux namespace 
 \Application::process();//$controller = new \Controllers\Article(); $controller->index();
 
 
