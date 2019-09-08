1a7d742795b019544f33aa861ddc4604b35decb7
unknown <a.andjib@yahoo.fr>
2019-09-08 22:21:33 +0200

    Un galere  pour r1. Le blem ct juste l'equilibre du nbre de paramettres entre la PREPARATION et l'EXECUTION de la requete

--- libraries/models/Connexion.php
+++ libraries/models/Connexion.php
@@ -100,9 +100,10 @@ abstract class Connexion {
     }
 
     public function editArticle($title, $slug,  $introduction, $content, $created_at, $nomPhoto, $id)
-    {                                             
+    {    
+        // Dans prepare(...)Au lieu de repeter 2 fois id = :id , On pourrait juste supprimer 'created_at" dans compact(...)                                       
         $query = $this->pdo->prepare("UPDATE {$this->table}  SET title = :title, slug = :slug, introduction = :introduction, content = :content, created_at = NOW(), photo = :nomPhoto, id = :id WHERE id = :id") ; // created_at = NOW ne compte pas parmis le nbre de paramettres                                                 
-        @$query->execute(compact('title','slug','introduction','content','created_at','nomPhoto','id' )); // Comme je tiens a garder 'created_at : en haut je repete 2 fois le  |id = :id | ce qui ne se fait pas formellement.   
+        @$query->execute(compact('title','slug','introduction','content','created_at','nomPhoto','id' )); // Comme je tiens a garder 'created_at : en haut je repete 2 fois le  |id = :id | - ce qui ne se fait pas formellement !.   
 
     }
 
